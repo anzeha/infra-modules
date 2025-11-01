9916e1862c02cdc35061f9aa89cf9c6334ae2e54
Anze <anze603@gmail.com>
2025-11-01 11:43:25 +0100

    chore: fixed sealed secrets repo url

--- kubernetes-addons/main.tf
+++ kubernetes-addons/main.tf
@@ -24,7 +24,7 @@ module "nginx-controller" {
 resource "helm_release" "sealed_secrets" {
   count            = var.install_sealed_secrets ? 1 : 0
   name             = "sealed-secrets"
-  repository       = "https://github.com/bitnami-labs/sealed-secrets/tree/main/helm/sealed-secrets"
+  repository       = "https://bitnami-labs.github.io/sealed-secrets"
   chart            = "sealed-secrets"
   create_namespace = true
 }
